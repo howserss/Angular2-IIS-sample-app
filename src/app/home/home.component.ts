2d03ff2938c50610fb8a4adbd4acf7fa3e1f1fc0
howserss <howserss@gmail.com>
2016-10-13 13:29:28 -0400

    Add files via upload

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,14 +10,40 @@ import { ActivatedRoute } from '@angular/router';
 export class HomeComponent implements OnInit {
 lastKey:string='(none)';
 account_id:string;
+orResult:string;
+andResult:string;
   constructor(private route:ActivatedRoute
 ) { }
 
   ngOnInit() {
     this.account_id=this.route.snapshot.params['account_id'];
     //alert(this.id);
+    this.orResult=this.testOr(this.account_id);
+    this.andResult=this.testAnd(this.account_id);
   }
 
 
+
+private testOr(it:string):string{
+
+  if( !(it == '1') || !(it == '2') || !(it == '3') || !(it == '4') )
+  {
+      return'Or Not in list';
+  }else{
+    return 'Or In List';
+  }
+}
+
+private testAnd(it:string):string{
+
+if( !(it == '1') && !(it == '2') && !(it == '3') && !(it == '4') )
+{
+    return 'AND Not in list';
+}else{
+    return 'And In List';
+}
+
+
+}
 
 }
